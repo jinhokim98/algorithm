1e31d0421f2df07519e43b18531ec25e856dde2b
JinHo Kim <81083461+jinhokim98@users.noreply.github.com>
2026-08-09 21:15:53 +0900

    [level 1] Title: K번째수, Time: 1.41 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for (int i = 0; i < commands.length; i++) {
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int k = commands[i][2];
+            int[] sliced = Arrays.copyOfRange(array, start - 1, end);
+            Arrays.sort(sliced);
+            answer[i] = sliced[k - 1];
+        }
+        return answer;
+    }
+}
